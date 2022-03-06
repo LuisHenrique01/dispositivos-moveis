3a95aa6470a35acbe006a4bf594b7dfcdcbf23bd
LuisHenrique01 <luizbueno1201@gmail.com>
2022-03-06 13:38:34 -0300

    Hello word add

---
+++ Exercicio 04/helloworld/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _text = '';
+  var _icon = const Icon(Icons.wb_incandescent);
+
+  void _showHelloWorld() {
+    setState(() {
+      if (_text == '') {
+        _text = 'Hello World!';
+        _icon = const Icon(Icons.wb_incandescent_outlined);
+      } else {
+        _text = '';
+        _icon = const Icon(Icons.wb_incandescent);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Precione e veja a mágica acontecer:',
+            ),
+            Text(
+              _text,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showHelloWorld,
+        tooltip: 'Show hello world',
+        child: _icon,
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
