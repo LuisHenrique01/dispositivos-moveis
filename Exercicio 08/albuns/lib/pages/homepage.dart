0f0aceb4962d7e848e7fa902d9326cfb6f33594b
LuisHenrique01 <luizbueno1201@gmail.com>
2022-03-31 16:33:10 -0300

    first commit

---
+++ Exercicio 08/albuns/lib/pages/homepage.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:albuns/requests/albuns_api.dart';
+import 'package:albuns/models/album.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Future<Album> futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ;
+  }
+}
